50803267e2a25ebcd58de8881dd97d28fcce78a6
Jonathan Bennett <jonny.bennett@gmail.com>
2014-06-22 02:28:41 +0100

    General syntax and styling changes.

--- src/api.js
+++ src/api.js
@@ -1,79 +1,79 @@
 // Node modules
 
 var https = require('https'),
-url = require('url');
+  url = require('url');
 // EdgeGrid Auth Module
 var auth = require('./auth.js');
 
 var _client_token = null,
-_client_secret = null,
-_access_token = null
-_request = null;
+  _client_secret = null,
+  _access_token = null,
+  _request = null;
 
-var EdgeGrid = function(client_token, client_secret, access_token) { 
+var EdgeGrid = function(client_token, client_secret, access_token) {
 
-	if(client_token == undefined || client_token == null) {
-		console.log("No client token");
-		return false;
-	} else if (client_secret == undefined || client_secret == null) {
-		console.log("No client secret");
-		return false;
-	} else if (access_token == undefined || access_token == null) {
-		console.log("No access token");
-		return false;
-	}
+  if (client_token === undefined || client_token === null) {
+    console.log("No client token");
+    return false;
+  } else if (client_secret === undefined || client_secret === null) {
+    console.log("No client secret");
+    return false;
+  } else if (access_token === undefined || access_token === null) {
+    console.log("No access token");
+    return false;
+  }
 
-	_client_token = client_token;
-	_client_secret = client_secret;
-	_access_token = access_token;
+  _client_token = client_token;
+  _client_secret = client_secret;
+  _access_token = access_token;
 
-	return this;
+  return this;
 
-}
+};
 
 EdgeGrid.prototype.auth = function(request, callback) {
-	_request = auth.generate_auth(request, _client_token, _client_secret, _access_token);
-	
-	if(callback != undefined) { 
-		callback(this);
-	}
+  _request = auth.generate_auth(request, _client_token, _client_secret, _access_token);
 
-	return this;
-}
+  if (callback && typeof callback == "function") {
+    callback(this);
+  }
 
-EdgeGrid.prototype.send = function(callback) {
+  return this;
+};
 
-	var request = _request,
-	data = "";
+EdgeGrid.prototype.send = function(callback) { 
 
-	var parts = url.parse(request.url);
-	request.hostname = parts.hostname,
-	request.port = parts.port,
-	request.path = parts.path;
+  var request = _request,
+    data = "";
 
-	if(request.method == "POST" || request.method == "PUT") { 
-		request.headers["content-type"] = 'application/x-www-form-urlencoded';
-		request.headers['content-length'] = request.body.length;
-	}
+  var parts = url.parse(request.url);
+  request.hostname = parts.hostname;
+  request.port = parts.port;
+  request.path = parts.path;
 
-	var req = https.request(request, function(res) {
-	  res.on('data', function(d) {
-	    data += d;
-	  });
+  if (request.method == "POST" || request.method == "PUT") {
+    request.headers["content-type"] = 'application/x-www-form-urlencoded';
+    request.headers['content-length'] = request.body.length;
+  }
 
-	  res.on('end', function () {
-	  	if(callback && typeof callback == "function") { 
-	  		callback(data, res);
-	  	}
-	    
-	  });
-	});
+  var req = https.request(request, function(res) {
+    res.on('data', function(d) {
+      data += d;
+    });
 
-	if(request.method == "POST" || request.method == "PUT") {
-		req.write(request.body);
-	}
+    res.on('end', function() {
+      if (callback && typeof callback == "function") {
+        callback(data, res);
+      }
 
-	req.end();
-}
+    });
+  });
+
+  if (request.method == "POST" || request.method == "PUT") {
+    req.write(request.body);
+  }
+
+  req.end();
+};
 
 module.exports = EdgeGrid;
--- src/auth.js
+++ src/auth.js
@@ -2,170 +2,170 @@
 // client_token + access_token + timestamp
 
 var uuid = require('node-uuid'),
-moment = require('moment'),
-crypto = require('crypto'), 
-_ = require('underscore'), 
-url = require('url'),
-log4js = require('log4js');
+  moment = require('moment'),
+  crypto = require('crypto'),
+  _ = require('underscore'),
+  url = require('url'),
+  log4js = require('log4js');
 
 // Setting up the logger
-log4js.replaceConsole()
+log4js.replaceConsole();
 // Set output level
 var logger = log4js.getLogger();
-logger.setLevel(log4js.levels.ERROR); ;
+logger.setLevel(log4js.levels.ERROR);
 
 var _headers_to_sign = null,
-_max_body = null;
+  _max_body = null;
 
-var createTimestamp = function() { 
-	var timestamp = moment().utc().format('YYYYMMDDTHH:mm:ss+0000');
-	return timestamp;
-} 
+var createTimestamp = function() {
+  var timestamp = moment().utc().format('YYYYMMDDTHH:mm:ss+0000');
+  return timestamp;
+};
 
 var base64_hmac_sha256 = function(data, key) {
-	var encrypt = crypto.createHmac("sha256", key);
-	encrypt.update(data);
-	return encrypt.digest("base64");
-}
+  var encrypt = crypto.createHmac("sha256", key);
+  encrypt.update(data);
+  return encrypt.digest("base64");
+};
 
 var canonicalize_headers = function(request) {
 
-	console.debug("HEADERS TO SIGN", _headers_to_sign)
-	var new_headers = [];
-	var cleansed_headers = {};
-	_.each(request.headers, function(value, header) {
-		if(value) { 
-			header = header.toLowerCase();
-			if(typeof value == "string") {
-				value = value.trim();
-				value = value.replace(/\s+/g, ' ');
-			}
-			
-			cleansed_headers[header.toLowerCase()] = value;				
-		}
-	});
+  console.debug("HEADERS TO SIGN", _headers_to_sign);
+  var new_headers = [];
+  var cleansed_headers = {};
+  _.each(request.headers, function(value, header) {
+    if (value) {
+      header = header.toLowerCase();
+      if (typeof value == "string") {
+        value = value.trim();
+        value = value.replace(/\s+/g, ' ');
+      }
 
-	_.each(_headers_to_sign, function(header, index) {
-		new_headers.push(header.toLowerCase()+":"+cleansed_headers[header.toLowerCase()])
-	});
+      cleansed_headers[header.toLowerCase()] = value;
+    }
+  });
 
-	
-	new_headers = new_headers.join("\t");
-	return new_headers;
-}
+  _.each(_headers_to_sign, function(header) {
+    new_headers.push(header.toLowerCase() + ":" + cleansed_headers[header.toLowerCase()]);
+  });
 
-var make_content_hash = function(request) { 
 
-		var max_body = _max_body;
+  new_headers = new_headers.join("\t");
+  return new_headers;
+};
 
-        var content_hash = "",
-        prepared_body = request.body || "";
+var make_content_hash = function(request) {
 
-        if(typeof prepared_body == "object") {
-        	console.info("body content is type object, transforming to post data");
-        	var post_data_new = "";
-			_.each(prepared_body, function(value, index) {
-				// console.log(encodeURIComponent(JSON.stringify(value)));
-				post_data_new += index + "=" + encodeURIComponent(JSON.stringify(value)) + "&";
-			});
-			prepared_body = post_data_new;
-			request.body = prepared_body;
-        }
+  var max_body = _max_body;
 
-        console.info("body is \"" + prepared_body + "\"");
-        console.debug("PREPARED BODY LENGTH", prepared_body.length)
+  var content_hash = "",
+    prepared_body = request.body || "";
 
-        if(request.method == "POST" && prepared_body.length > 0) {
-        	console.info("Signing content: \"" + prepared_body + "\"");
-        	// console.log(prepared_body.length);
-        	if(prepared_body.length > max_body) { 
-        		console.warn("Data length ("+prepared_body.length+") is larger than maximum "+max_body);
-        		prepared_body = prepared_body.substring(0, max_body);
-        		console.info("Body truncated. New value \"" +prepared_body + "\"");
-        	}
+  if (typeof prepared_body == "object") {
+    console.info("body content is type object, transforming to post data");
+    var post_data_new = "";
+    _.each(prepared_body, function(value, index) {
+      // console.log(encodeURIComponent(JSON.stringify(value)));
+      post_data_new += index + "=" + encodeURIComponent(JSON.stringify(value)) + "&";
+    });
+    prepared_body = post_data_new;
+    request.body = prepared_body;
+  }
 
-        	console.debug("PREPARED BODY", prepared_body);
+  console.info("body is \"" + prepared_body + "\"");
+  console.debug("PREPARED BODY LENGTH", prepared_body.length);
 
-        	var shasum = crypto.createHash('sha256');
-        	shasum.update(prepared_body);
-        	content_hash = shasum.digest("base64");
-        	console.info("Content hash is \""+content_hash+"\"");
+  if (request.method == "POST" && prepared_body.length > 0) {
+    console.info("Signing content: \"" + prepared_body + "\"");
+    // console.log(prepared_body.length);
+    if (prepared_body.length > max_body) {
+      console.warn("Data length (" + prepared_body.length + ") is larger than maximum " + max_body);
+      prepared_body = prepared_body.substring(0, max_body);
+      console.info("Body truncated. New value \"" + prepared_body + "\"");
+    }
 
-        	
-        }
+    console.debug("PREPARED BODY", prepared_body);
 
-        return content_hash;
-}
+    var shasum = crypto.createHash('sha256');
+    shasum.update(prepared_body);
+    content_hash = shasum.digest("base64");
+    console.info("Content hash is \"" + content_hash + "\"");
+
+
+  }
+
+  return content_hash;
+};
 
 var make_data_to_sign = function(request, auth_header) {
 
- 	var parsed_url = url.parse(request.url, true);
- 	var data_to_sign = [
- 	request.method.toUpperCase(),
- 	parsed_url.protocol.replace(":",""),
- 	parsed_url.host,
- 	parsed_url.path,
- 	canonicalize_headers(request),
- 	make_content_hash(request),
- 	auth_header
- 	].join("\t").toString();
+  var parsed_url = url.parse(request.url, true);
+  var data_to_sign = [
+    request.method.toUpperCase(),
+    parsed_url.protocol.replace(":", ""),
+    parsed_url.host,
+    parsed_url.path,
+    canonicalize_headers(request),
+    make_content_hash(request),
+    auth_header
+  ].join("\t").toString();
 
- 	console.info('data to sign: "'+ data_to_sign +'" \n')
+  console.info('data to sign: "' + data_to_sign + '" \n');
 
- 	return data_to_sign;
-}
+  return data_to_sign;
+};
 
 var sign_request = function(request, timestamp, client_secret, auth_header) {
-	return base64_hmac_sha256(make_data_to_sign(request, auth_header), make_signing_key(timestamp, client_secret));
-}
+  return base64_hmac_sha256(make_data_to_sign(request, auth_header), make_signing_key(timestamp, client_secret));
+};
 
-var make_signing_key = function(timestamp, client_secret) { 
+var make_signing_key = function(timestamp, client_secret) {
 
-    var signing_key = base64_hmac_sha256(timestamp, client_secret);
-    console.info("Signing key: "+signing_key+"\n");
-    return signing_key;
-}
+  var signing_key = base64_hmac_sha256(timestamp, client_secret);
+  console.info("Signing key: " + signing_key + "\n");
+  return signing_key;
+};
 
-var make_auth_header = function(request, client_token, access_token, client_secret, timestamp, nonce) { 
+var make_auth_header = function(request, client_token, access_token, client_secret, timestamp, nonce) {
 
- 	var key_value_pairs = {
- 		"client_token":client_token,
- 		"access_token":access_token,
- 		"timestamp":timestamp,
- 		"nonce":nonce
- 	}
+  var key_value_pairs = {
+    "client_token": client_token,
+    "access_token": access_token,
+    "timestamp": timestamp,
+    "nonce": nonce
+  };
 
- 	var joined_pairs = "";
- 	_.each(key_value_pairs, function(value, key) { 
- 		joined_pairs += key+"="+value+";";
- 	});
+  var joined_pairs = "";
+  _.each(key_value_pairs, function(value, key) {
+    joined_pairs += key + "=" + value + ";";
+  });
 
- 	var auth_header = "EG1-HMAC-SHA256 " + joined_pairs;
+  var auth_header = "EG1-HMAC-SHA256 " + joined_pairs;
 
- 	console.info("Unsigned authorization header: "+auth_header+"\n");
+  console.info("Unsigned authorization header: " + auth_header + "\n");
 
- 	var signed_auth_header = auth_header + "signature=" + sign_request(request, timestamp, client_secret, auth_header);
+  var signed_auth_header = auth_header + "signature=" + sign_request(request, timestamp, client_secret, auth_header);
 
- 	console.info("Signed authorization header: " + signed_auth_header+"\n");
+  console.info("Signed authorization header: " + signed_auth_header + "\n");
 
- 	return signed_auth_header;
+  return signed_auth_header;
 
 
-}
+};
 
 module.exports = {
-	generate_auth: function(request, client_token, client_secret, access_token, headers_to_sign, max_body, guid, timestamp) {
-		
-		_max_body = max_body || 2048;
-		_headers_to_sign = headers_to_sign || [];
-		 
-		var guid = guid || uuid.v4();
-		var timestamp = timestamp || createTimestamp();
+  generate_auth: function(request, client_token, client_secret, access_token, headers_to_sign, max_body, guid, timestamp) {
 
-		if(!request.hasOwnProperty("headers")) {
-			request.headers = {};
-		}
-		request.headers.Authorization = make_auth_header(request, client_token, access_token, client_secret, timestamp, guid);
-		return request;
-	}
-}
+    _max_body = max_body || 2048;
+    _headers_to_sign = headers_to_sign || [];
+
+    guid = guid || uuid.v4();
+    timestamp = timestamp || createTimestamp();
+
+    if (!request.hasOwnProperty("headers")) {
+      request.headers = {};
+    }
+    request.headers.Authorization = make_auth_header(request, client_token, access_token, client_secret, timestamp, guid);
+    return request;
+  }
+};
