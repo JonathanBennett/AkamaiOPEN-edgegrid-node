61629a88028dcb0fc3c7ec266af81c488e1cc699
Mike Ball <mikedball@gmail.com>
2015-07-29 08:40:20 -0400

    clean up some edgerc functionality

--- src/edgerc.js
+++ src/edgerc.js
@@ -1,37 +1,52 @@
 var fs = require('fs');
 
+function getGroup(lines, groupName) {
+  var match = /\[(.*)\]/,
+      lineMatch,
+      group;
+
+  lines.forEach(function(line, i) {
+    lineMatch = line.match(match);
+
+    if (lineMatch && lineMatch[1] === groupName) {
+      group = lines.slice(i + 1, i + 5);
+    }
+  });
+
+  return group;
+}
+
+function validatedConfig(config) {
+  if (config.host.indexOf('https://') > -1) {
+    return config;
+  }
+
+  config.host = 'https://' + config.host;
+
+  return config;
+}
+
+function buildObj(configs) {
+  var result = {},
+      keyVal;
+
+  configs.forEach(function(config) {
+    keyVal = config.split('=');
+
+    result[keyVal[0].trim()] = keyVal[1].trim();
+  });
+
+  return validatedConfig(result);
+}
+
 module.exports = function(path, conf) {
   var edgerc = fs.readFileSync(path).toString().split("\n"),
       confGroup = conf || 'default',
-      confData = [];
+      confData = getGroup(edgerc, confGroup);
 
-  for(var i=0;i<edgerc.length;i++) {
-    var matchConf = edgerc[i].match(/\[(.*)\]/);
-    // if we found our matching config, push the next 4 lines into a temp array
-    if (matchConf && matchConf[1] === confGroup) {
-      confData.push(edgerc[i+1]);
-      confData.push(edgerc[i+2]);
-      confData.push(edgerc[i+3]);
-      confData.push(edgerc[i+4]);
-      // convert the array to a descriptive object
-      confData = confData.map(function(el) {
-        var ret = {}
-        var key = el.split(' = ')[0].trim();
-        var val = el.split(' = ')[1].trim();
-        if (key === 'host') {
-          val = 'https://' + val;
-        }
-        ret[key] = val;
-        return ret;
-      });
-      // turn the array of objects into a single object
-      var result = {};
-      for (var i = 0, length = confData.length; i < length; i++) {
-        result[Object.keys(confData[i])[0]] = confData[i][Object.keys(confData[i])[0]];
-      }
-      return result;
-    }
+  if (!confData) {
+    throw('An error occurred parsing the .edgerc file. You probably specified an invalid group name.');
   }
-  // if we escaped the parse loop without returning, something is wrong
-  throw('An error occurred parsing the .edgerc file. You probably specified an invalid group name.');
+
+  return buildObj(confData);
 };
