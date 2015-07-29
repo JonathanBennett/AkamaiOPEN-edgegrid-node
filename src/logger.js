0713b80832c177f28a9bbbb89e479822afecb2fd
Mike Ball <mikedball@gmail.com>
2015-07-29 11:23:11 -0400

    correcting log level to ERROR

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,7 @@ var log4js = require('log4js'),
     logger = log4js.getLogger();
 
 if (!process.env.LOG4JS_CONFIG) {
-  logger.setLevel(log4js.levels.ALL);
+  logger.setLevel(log4js.levels.ERROR);
 }
 
 module.exports = logger;
